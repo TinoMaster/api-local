9d1f9c2bddf54f12454fdf020c1c2cb607ef25a4
TinoMaster <oscarmarcos2673@gmail.com>
2023-02-01 21:08:57 -0500

    fix

--- src/routes/mirones.routes.js
+++ src/routes/mirones.routes.js
@@ -11,7 +11,7 @@ const multer = require("multer");
 const storage = multer.diskStorage({
   destination: "uploads/",
   filename: function (req, file, cb) {
-    cb(null, file.originalname.split(" ")[0] + path.extname(file.originalname));
+    cb(null, /* file.originalname.split(" ")[0] */"Reporte_miron" + path.extname(file.originalname));
   },
 });
 const upload = multer({ storage });
