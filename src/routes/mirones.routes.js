247aa9cde2039990cdec9fe3b14afb0709e5b90a
TinoMaster <oscarmarcos2673@gmail.com>
2023-02-01 20:23:45 -0500

    Creado sistema que devuelve todos los datos de los excel

--- src/routes/mirones.routes.js
+++ src/routes/mirones.routes.js
@@ -6,13 +6,11 @@ const express = require("express"),
 
 const convertXLSX = require("../helpers/convertXLSX");
 const handlerCSV = require("../helpers/handlerCSV");
-let nameOfData;
 
 const multer = require("multer");
 const storage = multer.diskStorage({
   destination: "uploads/",
   filename: function (req, file, cb) {
-    nameOfData = file.originalname.split(" ")[0];
     cb(null, file.originalname.split(" ")[0] + path.extname(file.originalname));
   },
 });
@@ -24,12 +22,12 @@ router.post(
   "/mirones/handlerExcel",
   upload.single("file"),
   async (req, res) => {
+    const nameOfData = req.body.name;
     const archivo = req.file;
     convertXLSX(archivo.path)
       .then(() => {
         handlerCSV("public/csv/data.csv", nameOfData)
           .then((data) => {
-            
             res.json({
               success: true,
               message: "Archivo procesado correctamente",
